fee5cfd2122671378e42dc9fe6d3afa38396ee56
Tejas Kapadia <47652698+tkapadi1@users.noreply.github.com>
2021-02-03 19:29:57 -0500

    providing the functionalities

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
+
+function item() {
+  const [item, itemAdd] = useState("");
+
+  item = EventTarget.value;
+
+  function values() {
+    itemAdd();
+  }
+}
 
 function App() {
+  const [input, inputText] = useState("");
+  const [item, itemAdd] = useState([]);
+
+  function handleChange(event) {
+    const newVal = event.target.value;
+    inputText(newVal);
+  }
+
+  function button() {
+    itemAdd((previtems) => {
+      return [...previtems, input];
+    });
+    inputText("");
+  }
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
+        <input onChange={handleChange} type="text" value={input} />
+        <button onClick={button}>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>A Item </li>
+          {item.map((todoitem) => {
+            return <li> {todoitem} </li>;
+          })}
         </ul>
       </div>
     </div>
